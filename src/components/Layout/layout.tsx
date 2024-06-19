cfdf1ca78ca28ceeb45239c7a77b4eadb1690395
Wicki Krzysztof <krzysiekwicki1@gmail.com>
2024-06-19 13:03:38 +0200

    feat: update page style

--- src/components/Layout/layout.tsx
+++ src/components/Layout/layout.tsx
@@ -1,11 +1,11 @@
 export default function Layout({ sidebar, children }: any) {
   return (
-    <div className="container mx-auto flex bg-gradient bg-repeat pt-16">
+    <div className="bg-gradient container mx-auto flex bg-repeat pt-16">
       <aside className="sticky top-16 h-[calc(100vh-5rem)] self-start overflow-y-auto">
         {sidebar}
       </aside>
       <main className="flex-1">
-        <div className="mx-4 mb-4 flex flex-col space-y-2 rounded-xl bg-white p-8 px-4 pt-6 border-[3px] border-primary">
+        <div className="mx-4 mb-4 flex flex-col space-y-2 rounded-xl border-2 border-primary bg-white p-8 px-4 pt-6">
           {children}
         </div>
       </main>
