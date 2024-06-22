36dc8e5b8fd8966b5d509f7b1f8209183578d61a
Wicki Krzysztof <krzysiekwicki1@gmail.com>
2024-06-22 10:33:55 +0200

    feat: update button

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -94,7 +94,7 @@ export default {
         slide: "slide var(--speed) ease-in-out infinite alternate",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        shine: "shine 8s ease-in-out infinite",
+        shine: "shine 6s linear infinite",
       },
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
